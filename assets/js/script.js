9b9fb0ead21725025b53add9ff0f3890cfaa4b90
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-05-02 17:45:33 -0400

    cleaned up quiz area HTML
    refactored quiz data into an object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,71 @@
+var quizObj = {
+    questionData: {
+        1: {
+            question: "What does the acronym JSON mean?",
+            answers: {
+                "Javascript Object Notation": true,
+                "Javascript Oriented Node": false,
+                "Javascript Object Node": false,
+                "Javascript Oriented Notation": false
+            },
+        },
+        2: {
+            question: "What does the acronym HTML mean?",
+            answers: {
+                "Host Token Medial Layer": false,
+                "Hypertext Model Linux": false,
+                "Hypertext Markup Language": true,
+                "HTTP Messenge Length": false
+            },
+        },
+        3: {
+            question: "What does the acronym CSS mean?",
+            answers: {
+                "Computing Screen Selector": false,
+                "Community Styling Standards": false,
+                "Complete Style Source": false,
+                "Cascading Style Sheet": true
+            },
+        },
+    },
+    // Ask Quiz Questions function
+    cycleQuestions: (quizArea) => {
+        // for every property in object run display for...in
+        for (var questionNo in quizObj["questionData"]) {
+            // question string being asked
+            var currentQuestion = quizObj["questionData"][questionNo]["question"];
+            
+            // display question in the quiz area
+            quizArea.append(document.createElement("p").textContent = currentQuestion);
+            // debugger;
+
+            // display multiple choice answers
+            quizArea.append(document.createElement("select").textContent = "** Area for multiple choice answers **");
+        }
+    }
+}
+
+var quizBox = function (currentQuestion, currentRightAnswer, option1, option2, option3) {
+    
+}
+
 /* GLOBAL VARIABLES */
-// HTML elements
-var startBtn = document.querySelector("#startBtn");
 var timer = document.querySelector("#timer");
+var quizArea = document.querySelector("#quizArea");
+var startBtn = document.querySelector("#startBtn");
+
 // Questions & Answers object
 var quizMaterial = {
     "What does the acronym JSON mean?":"Javascript Object Notation",
     "What does the acronym HTML mean?":"Hypertext Markup Language",
     "What does the acronym  CSS mean?":"Cascading Style Sheet"
 }
-// Function: Begin Countdown
+
+// BEGIN COUNTDOWN function
 var countdownTimer = function () {
     // default start time in seconds
     var defaulTimeLeft = 60;
-    // deduct 1 sec from time left every second 
+    // deduct 1 sec from time left every second
     setInterval(function () {
         if (defaulTimeLeft > 1) {
             defaulTimeLeft--;
@@ -22,22 +75,13 @@ var countdownTimer = function () {
         }
     }, 1000)
 };
-// Function: Ask Quiz Questions
-var cycleQuestions = () => {
-    var quizArea = document.querySelector("#quizArea");
 
-    for (var question in quizMaterial) {
-        var currentQuestion = document.createElement("p").textContent = question;
-
-        quizArea.prepend(currentQuestion);
-    }
-};
-// Function: Execute application
+// Execute application function
 var startQuiz = function () {
     // begin the timer
     countdownTimer();
     // ask user questions
-    cycleQuestions();
+    quizObj.cycleQuestions(document.querySelector("#quizArea"));
 }
 
 // Start quiz when click event triggered
