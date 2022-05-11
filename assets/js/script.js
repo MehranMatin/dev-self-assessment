568c959a89778e13125fbd4f173ea85a2821253a
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-05-11 17:45:38 -0400

    removed dynamically created elements
    fixed next question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,129 +1,102 @@
 /* GLOBAL VARIABLES */
 var quizArea = document.getElementById('quizArea');
+var currentQuestion = 0;
+var currentScore = 0;
+var defaulTimeLeft = 60;
+var interval = null;
 ////////////////////////////////////////////////
 
 var quizObj = {
     questionData: [
         {
             question: "What does the acronym JSON mean?",
-            answers: {
-                "Javascript Object Notation": true,
-                "Javascript Oriented Node": false,
-                "Javascript Object Node": false,
-                "Javascript Oriented Notation": false
-            }
+            answers: [
+                "Javascript Object Notation",
+                "Javascript Oriented Node",
+                "Javascript Object Node",
+                "Javascript Oriented Notation"
+            ],
+            correctAnswer: 0
         },
         {
             question: "What does the acronym HTML mean?",
-            answers: {
-                "Host Token Medial Layer": false,
-                "Hypertext Model Linux": false,
-                "Hypertext Markup Language": true,
-                "HTTP Messenge Length": false
-            }
+            answers: [
+                "Host Token Medial Layer",
+                "Hypertext Model Linux",
+                "Hypertext Markup Language",
+                "HTTP Messenge Length"
+            ],
+            correctAnswer: 2
         },
         {
             question: "What does the acronym CSS mean?",
-            answers: {
-                "Computing Screen Selector": false,
-                "Community Styling Standards": false,
-                "Complete Style Source": false,
-                "Cascading Style Sheet": true
-            }
+            answers: [
+                "Computing Screen Selector",
+                "Community Styling Standards",
+                "Complete Style Source",
+                "Cascading Style Sheet"
+            ],
+            correctAnswer: 3
         },
     ],
     ////////////////////////////////////////////////
     hideQuizIntro: () => {
         document.getElementById('introArea').classList.remove('d-flex');
         document.getElementById('introArea').classList.add('d-none');
+
+        document.getElementById('quizArea').style.display = 'block';
     },
     countdownTimer: () => {
-        // default start time in seconds
-        var defaulTimeLeft = 60;
         // deduct 1 sec from time left every second
-        setInterval(function () {
+        interval = setInterval(function () {
             if (defaulTimeLeft > 1) {
                 defaulTimeLeft--;
                 document.querySelector("#timer").textContent = `Time Remaining: ${defaulTimeLeft} seconds`;
             } else {
                 document.querySelector("#timer").textContent = `Time's Up!`;
+                clearInterval();
+                quizObj.endQuiz();
             }
         }, 1000)
-    },
-    createElPlaceholders: (quizArea) => {
-        // create <h3> element for question #
-        var h3El = document.createElement("h3");
-        h3El.setAttribute('id','questionNumberPlaceholder');
-        h3El.setAttribute('class','px-5 py-3 text-light bg-dark');
-        h3El.textContent = 'Question # placeholder';
-
-        // create <h1> element for question placeholder
-        var h1El = document.createElement("h1");
-        h1El.setAttribute('id','questionPlaceholder');
-        h1El.setAttribute('class','px-5 py-3 m-3 border-bottom border-dark');
-        h1El.style.cssText = "font-weight: bold";
-        h1El.textContent = 'Question text string placeholder?';
-
-        // create ordered list to hold answer <li> elements
-        var olEl = document.createElement("ol");
-        olEl.setAttribute('id','answersListPlaceholder');
-        olEl.setAttribute('class','p-5 m-3');
-        olEl.style.cssText = "background-color: light-green";
-
-        // display question <h1> and answers <ol> elements in the quiz area
-        quizArea.append(h3El);
-        quizArea.append(h1El);
-        quizArea.append(olEl);
     },
     nextQuestion: () => {
         // array of question data objects
         var quizQuestions = quizObj["questionData"];
 
-        // for each object in array...
-        for (var i = 0; i < quizQuestions.length; i++) {
-            var validAnswer = false;
+        // <h3> placeholder for current question number
+        document.getElementById("questionNumberPlaceholder").textContent = 'Question #' + (currentQuestion+1);
 
-            if (!validAnswer) {
-                // <h3> placeholder for current question number
-                document.getElementById("questionNumberPlaceholder").textContent = 'Question #' + (i+1);
+        // <h1> placeholder for current question string 
+        var questionPlaceholder = document.getElementById("questionPlaceholder");
+        // assign question string to <h1> placeholder
+        questionPlaceholder.textContent = quizQuestions[currentQuestion]["question"];
+        
+        // new array of answer strings
+        var possibleAnswers = quizQuestions[currentQuestion]["answers"];
+        console.log(possibleAnswers);
+        // loop through the new array
+        for (var i = 0; i < possibleAnswers.length; i++) {
+            // assign each answer string to a new <button> element...
+            document.getElementById('choice_' + (i + 1)).textContent = possibleAnswers[i];
+        }
+    },
+    checkAnswer: () => {
+        var answer = event.target.textContent;
+        if (answer === quizObj.questionData[currentQuestion].answers[quizObj.questionData[currentQuestion].correctAnswer]) {
+            currentScore++;
+            document.querySelector('#result').innerHTML = 'correct!';
+        } else {
+            defaulTimeLeft -= 5;
+            document.querySelector('#result').innerHTML = 'incorrect!';
+        }
 
-                // <h1> placeholder for current question string 
-                var questionPlaceholder = document.getElementById("questionPlaceholder");
-                // assign question string to <h1> placeholder
-                questionPlaceholder.textContent = quizQuestions[i]["question"];
-                
-                // <ol> placeholder for list of current answers
-                var answersListPlaceholder = document.getElementById("answersListPlaceholder");
+        currentQuestion++;
 
-                // new array of answer strings using each key of answers object
-                var possibleAnswers = Object.keys(quizQuestions[i]["answers"]);
-
-                // loop through the new array to dynamically create new li>button
-                for (var i; i < possibleAnswers.length; i++) {
-                    // assign each answer string to a new <button> element...
-                    var buttonEl = document.createElement("button");
-                    buttonEl.setAttribute('id','choice_#' + (i + 1));
-                    buttonEl.setAttribute('class','answerOption m-1 p-3 w-75 rounded text-left');
-                    buttonEl.textContent = possibleAnswers[i];
-                    console.log(buttonEl.id);
-
-                    // ...then nest inside a new <li>
-                    var liEl = document.createElement("li");
-                    liEl.append(buttonEl);
-                    
-                    // ...then nest inside placeholder <ol>
-                    answersListPlaceholder.appendChild(liEl);
-                }
-
-                // event listener for answer click to reset new question
-                var answerOption = document.querySelectorAll('button.answerOption');
-
-                for (var i = 0; i < answerOption.length; i++) {
-                    answerOption[i].addEventListener("click", function() {
-                        validAnswer = true;
-                    });
-                }
-            }
+        if (currentQuestion < quizObj.questionData.length) {
+            quizObj.nextQuestion();
+        } else {
+            quizObj.endQuiz();
+            clearInterval(interval);
         }
     },
     endQuiz: () => {
@@ -131,6 +104,8 @@ var quizObj = {
     // show the final score
     // submit high score
     document.getElementById('quizArea').style.display = 'none';
+    document.getElementById('finalScore').style.display = 'block';
+    document.getElementById('finalScore').innerHTML = '<p>Final Score: ' + currentScore + '/' + quizObj.questionData.length + '</p>';
     }
 }
 
@@ -142,11 +117,14 @@ var startQuiz = function () {
     quizObj.hideQuizIntro();
     // begin the timer
     quizObj.countdownTimer();
-    // dynamically create placeholders
-    quizObj.createElPlaceholders(document.querySelector("#quizArea"));
     // ask user questions
     quizObj.nextQuestion();
 }
 
 // Start quiz when click event triggered
 document.querySelector("#startBtn").addEventListener("click", startQuiz);
+
+document.querySelector("#choice_1").addEventListener("click", quizObj.checkAnswer);
+document.querySelector("#choice_2").addEventListener("click", quizObj.checkAnswer);
+document.querySelector("#choice_3").addEventListener("click", quizObj.checkAnswer);
+document.querySelector("#choice_4").addEventListener("click", quizObj.checkAnswer);
